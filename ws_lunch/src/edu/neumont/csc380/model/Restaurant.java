21691df151c6bee548b5c5d7040dc3102b5716ff
cbacon91 <cbacon@BACON3959.Student.Northface.local>
2013-07-25 17:38:43 -0600

    Updated model classes and xsd

--- ws_lunch/src/edu/neumont/csc380/model/Restaurant.java
+++ ws_lunch/src/edu/neumont/csc380/model/Restaurant.java
@@ -1,11 +1,123 @@
+
 package edu.neumont.csc380.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
 /**
- * Created with IntelliJ IDEA.
- * User: sgomez
- * Date: 7/23/13
- * Time: 7:09 PM
- * To change this template use File | Settings | File Templates.
+ * <p>Java class for restaurant complex type.
+ *
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ *
+ * <pre>
+ * &lt;complexType name="restaurant">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="address" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="menuitems" type="{}menuItem" maxOccurs="unbounded" minOccurs="0"/>
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ *
+ *
  */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "restaurant", propOrder = {
+        "address",
+        "menuitems",
+        "name"
+})
 public class Restaurant {
+
+    protected String address;
+    @XmlElement(nillable = true)
+    protected List<MenuItem> menuitems;
+    protected String name;
+
+    /**
+     * Gets the value of the address property.
+     *
+     * @return
+     *     possible object is
+     *     {@link String }
+     *
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * Sets the value of the address property.
+     *
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *
+     */
+    public void setAddress(String value) {
+        this.address = value;
+    }
+
+    /**
+     * Gets the value of the menuitems property.
+     *
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the menuitems property.
+     *
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getMenuitems().add(newItem);
+     * </pre>
+     *
+     *
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link MenuItem }
+     *
+     *
+     */
+    public List<MenuItem> getMenuitems() {
+        if (menuitems == null) {
+            menuitems = new ArrayList<MenuItem>();
+        }
+        return this.menuitems;
+    }
+
+    /**
+     * Gets the value of the name property.
+     *
+     * @return
+     *     possible object is
+     *     {@link String }
+     *
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the value of the name property.
+     *
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
 }
